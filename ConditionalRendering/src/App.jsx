211267299b13c7a4469c4027331fe15844507d2c
vipulkumar82 <vipulkumar212003@gmail.com>
2025-05-11 11:07:21 +0530

    if else conditional

---
+++ ConditionalRendering/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import LoginBtn from './Components/LoginBtn';
+import LogoutBtn from './Components/LogoutBtn';
+
+function App() {
+  const [isLoggedIn, setLoggedIn]=useState(true);
+
+    //if else conditional Statement
+
+      // if(isLoggedIn){
+      //   return(
+      //     <div className="main">
+      //     <h1>JayShriRam</h1>
+      //     <LogoutBtn/>
+      //     </div>
+      //   )
+      // }
+      // else{
+      //   return(
+      //     <div className="main">
+      //     <h1>JayShriRam</h1>
+      //     <LoginBtn/>
+      //     </div>
+      //   )
+      // }
+
+}
+
+export default App
