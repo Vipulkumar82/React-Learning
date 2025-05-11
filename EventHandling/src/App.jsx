17f05b555764ef6a5be0236d30c6adce31a8a786
vipulkumar82 <vipulkumar212003@gmail.com>
2025-05-11 23:25:08 +0530

    Worked on Event Handling

---
+++ EventHandling/src/App.jsx
@@ -0,0 +1,35 @@
+import './App.css'
+
+function App() {
+  function handleClick(){
+    alert("Button Clicked...");
+  }
+
+  function handleMouseOver(){
+    alert("Mouse is Over the paraGraph");
+  }
+  
+  function handleInputChange(e){
+    console.log(e.target.value);
+  }
+  function handleSubmit(e){
+    e.preventDefault();
+    alert("form Submitted...")
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleInputChange} />
+        <button type='submit'>Submit</button>
+      </form>
+
+
+
+      {/* <p onMouseOver={handleMouseOver} >This is Para</p>
+
+      <button onClick={handleClick} >Click Here</button> */}
+    </div>
+  )
+}
+
+export default App
