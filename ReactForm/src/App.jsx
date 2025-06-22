9ca69c0e35e687d3ae42978ccf901d816d2d7512
vipulkumar82 <vipulkumar212003@gmail.com>
2025-06-22 23:09:58 +0530

    Learned about form handling

--- ReactForm/src/App.jsx
+++ ReactForm/src/App.jsx
@@ -7,25 +7,40 @@ function App() {
     register,
     handleSubmit,
     watch,
-    formState:{errors},
+    formState:{errors, isSubmitting},
 
   } = useForm();
 
   function onSubmit(data) {
-    console.log(data);
+    console.log("Submitting Data",data);
   }
+
+  async function onSubmit(data){
+    await new Promise((resolve)=> setTimeout(resolve, 5000));
+    console.log("Submitting Data",data);
+    alert("Form Submitted Successfully");
+  }
+
 
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)} >
         <div>
           <label>First Name: </label>
-          <input {...register('firstName')} />
+          <input
+          className={errors.firstName ? 'input-error':""}
+          {...register('firstName',{ 
+            required: true, 
+            minLength:{value:3, message:'Minimum Length should be 3'},
+            maxLength: {value:6, message:'Maximum Length Should be 6'}})} />
+            {errors.firstName && <p className='error-msg' >{errors.firstName.message}</p>}
         </div>
         <br/>
         <div>
           <label>Middle Name: </label>
           <input {...register('middleName')} />
+
+
         </div>
         <br/>
         <div>
@@ -33,7 +48,8 @@ function App() {
           <input {...register('lastName')} />
         </div>
         <br/>
-        <input type="submit" />
+        <input type="submit" disabled={isSubmitting} 
+        value={isSubmitting ? 'Submitting' : "Submit"}/>
       </form>
     </>
   )
